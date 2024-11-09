eb6a006e19cd63594814f86ebcc53f839e3b5fd5
LiHogg <123923344+LiHogg@users.noreply.github.com>
2024-11-09 16:43:12 +0300

    Add files via upload

---
+++ Modele_4_1.py
@@ -0,0 +1,18 @@
+from fake_math import divide as div
+from true_math import divide as tdiv
+
+result1 = div(69, 3)
+
+result2 = div(3, 0)
+
+result3 = tdiv(49, 7)
+
+result4 = tdiv(15, 0)
+
+print(result1)
+
+print(result2)
+
+print(result3)
+
+print(result4)
---
+++ fake_math.py
@@ -0,0 +1,4 @@
+def divide(first, second):
+    if second == 0:
+        return "ошибка"
+    else: return first/second
